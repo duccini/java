ba717ba86b0ac5916267451219fe09287d33c71f
duccini <guilhermeduccini@gmail.com>
2024-05-10 21:13:11 -0300

    feat: create package aula07

---
+++ curso-java-basico/src/loiane/aula10/aula10.java
@@ -0,0 +1,9 @@
+package loiane.aula10;
+
+public class aula10 {
+    public static void main(String[] args) {
+        int num = 10;
+
+        System.out.println(("Hello World: " + num));
+    }
+}
