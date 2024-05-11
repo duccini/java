46800a54c1051977096947d89700dd84384cfad3
duccini <guilhermeduccini@gmail.com>
2024-05-11 20:29:59 -0300

    primitive types

---
+++ curso-java-basico/src/loiane/cursojava/aula11/curiosidadeInt.java
@@ -0,0 +1,13 @@
+package loiane.cursojava.aula11;
+
+public class curiosidadeInt {
+
+    public static void main(String[] args) {
+        int var1 = 2147483647;
+        // valor máximo do inteiro
+        // o proximo valor é o valor inteiro minimo, funcionando como uma roleta -2147483647
+        int var2 = 100;
+
+        System.out.println(var1 + var2);
+    }
+}
