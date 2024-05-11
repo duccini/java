75ce014f6e5f38f292d461a818f98228292e02ec
duccini <guilhermeduccini@gmail.com>
2024-05-11 20:11:03 -0300

    primitive types

---
+++ curso-java-basico/src/loiane/cursojava/aula11/VarBoolean.java
@@ -0,0 +1,14 @@
+package loiane.cursojava.aula11;
+
+public class VarBoolean {
+
+    public static void main(String[] args) {
+        boolean verdadeiro = true;
+        boolean falso = false;
+
+        System.out.println("O valor de Verdadeiro é " + verdadeiro);
+        System.out.println("O valor de Falso é " + falso);
+    }
+
+
+}
---
+++ curso-java-basico/src/loiane/cursojava/aula11/VarString.java
@@ -0,0 +1,15 @@
+package loiane.cursojava.aula11;
+
+public class VarString {
+    public static void main(String[] args) {
+        //char o = 'O';
+        //char i = 'i';
+
+        char o = 111;
+        char i = 105;
+        char inter = 0x00E1; // Unicode ?
+
+
+        System.out.println("" + o + i + inter);
+    }
+}
