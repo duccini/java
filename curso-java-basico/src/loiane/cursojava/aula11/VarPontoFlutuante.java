71a6cc59ec7b0bec8a3bb9aa634daa82e58c5afe
duccini <guilhermeduccini@gmail.com>
2024-05-11 18:54:40 -0300

    primitive types

---
+++ curso-java-basico/src/loiane/cursojava/aula11/VarPontoFlutuante.java
@@ -0,0 +1,11 @@
+package loiane.cursojava.aula11;
+
+public class VarPontoFlutuante {
+    public static void main(String[] args) {
+        float valorTomate = 3.95f;
+        double valorPassagem = 5.90;
+
+        System.out.println("Valor do tomate: " + valorTomate);
+        System.out.println("Valor do passagem: " + valorPassagem);
+    }
+}
---
+++ curso-java-basico/src/loiane/cursojava/aula11/VariaveisInteiras.java
@@ -0,0 +1,12 @@
+package loiane.cursojava.aula11;
+
+public class VariaveisInteiras {
+    public static void main(String[] args) {
+        byte idade1 = 20;
+        short idade = 21;
+        int idade3 = 22;
+        long idade4 = 23;
+
+        System.out.println("Valor variável idade1: " + idade1);
+    }
+}
