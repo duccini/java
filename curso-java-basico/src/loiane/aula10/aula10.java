12a5914a5a3ef666d1793470830d302c7fa1d01c
duccini <guilhermeduccini@gmail.com>
2024-05-10 23:11:03 -0300

    commit

--- curso-java-basico/src/loiane/aula10/aula10.java
+++ curso-java-basico/src/loiane/aula10/aula10.java
@@ -3,7 +3,9 @@ package loiane.aula10;
 public class aula10 {
     public static void main(String[] args) {
         int num = 10;
+        String nome = "Gui";
 
-        System.out.println(("Hello World: " + num));
+
+        System.out.println(("Hello World: " + nome));
     }
 }
