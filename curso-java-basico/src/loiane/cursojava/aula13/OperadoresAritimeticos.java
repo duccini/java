8959fe9c066a7f219a774aaa228b40ab81975b36
duccini <guilhermeduccini@gmail.com>
2024-05-26 15:52:53 -0300

    aula13: operadores

--- curso-java-basico/src/loiane/cursojava/aula13/OperadoresAritimeticos.java
+++ curso-java-basico/src/loiane/cursojava/aula13/OperadoresAritimeticos.java
@@ -2,15 +2,15 @@ package loiane.cursojava.aula13;
 
 public class OperadoresAritimeticos {
     public static void main(String[] args) {
-        int resultado = 1+ 2;
+        int resultado = 1 + 2;
 
         // 3
         System.out.println(resultado);
 
-        // 3, Imprime e depois adiciona 1
+        // 3, Imprime e depois adiciona 1, pos fixado
         System.out.println(resultado++);
 
-        // 5, Adiciona 1 e depois imprime
+        // 5, Adiciona 1 e depois imprime, pre fixado
         System.out.println(++resultado);
     }
 
