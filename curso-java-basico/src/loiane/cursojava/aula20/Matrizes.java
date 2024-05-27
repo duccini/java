8041b1d201158ba5679929c5fecd6fb09b7ccbfd
duccini <guilhermeduccini@gmail.com>
2024-05-27 16:35:18 -0300

    aula20: matrizes

---
+++ curso-java-basico/src/loiane/cursojava/aula20/Matrizes.java
@@ -0,0 +1,17 @@
+package loiane.cursojava.aula20;
+
+public class Matrizes {
+    public static void main (String[] args) {
+        double[][] notasAlunos = new double[30][4]; // 30 alunos, 4 notas
+
+        notasAlunos[0][0] = 10;
+        notasAlunos[0][1] = 7;
+        notasAlunos[0][2] = 9;
+        notasAlunos[0][3] = 9.5;
+
+        notasAlunos[1][0] = 9;
+        notasAlunos[1][1] = 8;
+        notasAlunos[1][2] = 7;
+        notasAlunos[1][3] = 9.5;
+    }
+}
