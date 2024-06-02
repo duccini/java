98ed4a5ff4a2f89996df5aa32784aca3c5e27b87
duccini <guilhermeduccini@gmail.com>
2024-06-02 20:15:52 -0300

    programarcomvoce: lista 1

--- ProgramarComVoce/src/programarcomvoce/exercicios/lista1/Exercicio5.java
+++ ProgramarComVoce/src/programarcomvoce/exercicios/lista1/Exercicio5.java
@@ -1,4 +1,24 @@
 package programarcomvoce.exercicios.lista1;
 
+import java.util.Scanner;
+
 public class Exercicio5 {
+    public static void main (String[] args) {
+        String[] diasDaSemana = {"Domingo", "Segunda-feira", "Terça-feira", "Quarta-feita", "Quinta-feira", "Sexta-feira", "Sábado"};
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite um número inteiro de 1 a 7");
+
+        int dia = scan.nextInt();
+
+        if (dia > 7 || dia < 1) {
+            System.out.println("O dia não é válido");
+        } else {
+            System.out.println(diasDaSemana[dia - 1]);
+        }
+
+
+    }
+
 }
--- ProgramarComVoce/src/programarcomvoce/exercicios/lista1/Exercicio6.java
+++ ProgramarComVoce/src/programarcomvoce/exercicios/lista1/Exercicio6.java
@@ -1,4 +1,12 @@
 package programarcomvoce.exercicios.lista1;
 
 public class Exercicio6 {
+    public static void main (String[] args) {
+        double produto = 1;
+        for(int i = 15; i <= 30; i = i + 2) {
+            produto = produto * i;
+
+        }
+        System.out.println(produto);
+    }
 }
